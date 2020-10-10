df12d551c8988250ab9290bc9b42eecd863c245d
Leo Crapart <leocrapart@yahoo.fr>
2020-10-11 00:21:54 +0200

    1

---
+++ src/layouts/Default.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <header>
+      <strong>
+        <g-link to="/">{{ $static.metadata.siteName }}</g-link>
+      </strong>
+      <nav>
+        <g-link to="/">Home</g-link>
+        <g-link to="/about/">About</g-link>
+      </nav>
+    </header>
+    <slot/>
+  </div>
+</template>
+
+<static-query>
+query {
+  metadata {
+    siteName
+  }
+}
+</static-query>
+
